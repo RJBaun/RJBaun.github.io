bd705e1a4dc079d63f8125ebb8f877066376db4f
Rylan Baun <rylanjbaun@gmail.com>
2024-05-21 23:35:52 -0600

    Bug fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer>
       <p>This page created by Rylan Baun - Last updated May 21, 2024</p>
-      <p>Repo can be found at https://github.com/RJBaun/RJBaun.github.io</p> 
+      <p>Repo can be found at <a href=''>https://github.com/RJBaun/RJBaun.github.io</a></p> 
     </footer>
   )
 }
